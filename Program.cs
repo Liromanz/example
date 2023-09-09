9f8566d097cd6d8245f1e3e8b73b78b34d77d3a1
Liromanz <kiss_wiwa_2003@mail.ru>
2023-09-09 13:26:54 +0300

    тренируемся отправлять на гит

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*Console.WriteLine("Введите сколько сахара вы хотите");
+string input = Console.ReadLine();
+int num = Convert.ToInt32(input);
+
+int kolvo = 1;
+
+while (kolvo <= num)
+{
+    Console.WriteLine("Добавили" + kolvo + "ложек сахара");
+    kolvo++;
+}*/
+
+int num;
+do
+{
+
+    Console.WriteLine("Какой цвет вы любите?");
+    Console.WriteLine("1. Красный");
+    Console.WriteLine("2. Голубой");
+    Console.WriteLine("3. Салатовый");
+    Console.WriteLine("4. Фиолетовый");
+    Console.WriteLine("5. Выход");
+
+    //1 вариант
+    //string input = Console.ReadLine();
+    //int num = Convert.ToInt32(input);
+
+    //2 вариант
+    //int num = Convert.ToInt32(Console.ReadLine());
+
+    //3 вариант
+    int.TryParse(Console.ReadLine(), out num);
+} while (num != 5);
+
